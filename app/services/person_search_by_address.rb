179f4dc0132c15224c17f6a63a766ddc07aacf90
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-01-30 20:34:02 -0800

    ruby lint fix;

--- app/services/person_search_by_address.rb
+++ app/services/person_search_by_address.rb
@@ -24,15 +24,14 @@ module PersonSearchByAddress
   end
 
   def should
-    [{ "nested": { "path": 'addresses', "query": { "bool": {
-      "should": [
-        match_query('addresses.autocomplete_searchable_address', @street, operator: 'and', boost: HIGH_BOOST),
-        match_query('addresses.last_known', 'true', boost: HIGH_BOOST),
-        match_query('addresses.autocomplete_city', @city, boost: HIGH_BOOST),
-        match_query('addresses.county.description', @county, boost: HIGH_BOOST),
-        match_query('addresses.searchable_address', @street, boost: HIGH_BOOST),
-        match_query('addresses.city', @city, boost: HIGH_BOOST)
-      ].compact
-    } } } }]
+    [{ "nested": { "path": 'addresses', "query": { "bool": { "should": [
+      match_query('addresses.autocomplete_searchable_address', @street,
+        operator: 'and', boost: HIGH_BOOST),
+      match_query('addresses.last_known', 'true', boost: HIGH_BOOST),
+      match_query('addresses.autocomplete_city', @city, boost: HIGH_BOOST),
+      match_query('addresses.county.description', @county, boost: HIGH_BOOST),
+      match_query('addresses.searchable_address', @street, boost: HIGH_BOOST),
+      match_query('addresses.city', @city, boost: HIGH_BOOST)
+    ].compact } } } }]
   end
 end
