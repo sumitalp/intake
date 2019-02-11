9f7042fc4c9f5c681242f56ed53b07bc45535cbf
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2019-02-11 10:42:07 -0800

    (FIT-434) - fixing lint error "Line is too long"

--- scripts/ci/rspec_test.rb
+++ scripts/ci/rspec_test.rb
@@ -4,7 +4,7 @@
 test_args = "-p intake_accelerator#{ENV['BUILD_NAME']}_test -f docker/test/docker-compose.yml"
 webpack = 'bin/webpack'
 
-exclude = ENV['EXCLUDE_PATTERN'] ? '--exclude-pattern ' + ENV['EXCLUDE_PATTERN'] : ''
-rspec_cmd = "#{webpack} && bundle exec parallel_rspec --runtime-log parallel_runtime_rspec.log #{exclude} spec"
+excl = ENV['EXCLUDE_PATTERN'] ? '--exclude-pattern ' + ENV['EXCLUDE_PATTERN'] : ''
+rspec_cmd = "#{webpack} && bundle exec parallel_rspec --runtime-log parallel_runtime_rspec.log #{excl} spec"
 
 exec("docker-compose #{test_args} run run_tests bash -c \"#{rspec_cmd}\"")
