e6aa46c7e2da16d2813946af3a79ae10363778c3
manish pokharel <manish.pokharel@osi.ca.gov>
2019-02-20 14:13:49 -0800

    SNAP-931 doesnot display advanced search in screening

--- spec/javascripts/configSpec.js
+++ spec/javascripts/configSpec.js
@@ -1,4 +1,4 @@
-import {isFeatureActive, isFeatureInactive, config, jsClipboardSupported, sdmPath, isAdvancedSearchOn} from 'common/config'
+import {isFeatureActive, isFeatureInactive, config, jsClipboardSupported, sdmPath, isAdvancedSearchOn, isSearchByAddressOn} from 'common/config'
 
 describe('intake config', () => {
   let windowOrg
@@ -88,14 +88,32 @@ describe('intake config', () => {
   })
 
   describe('.isAdvancedSearchOn', () => {
-    it('display advanced search when path includes screenings', () => {
+    it('doesnot display advanced search when path includes screenings', () => {
       window.org = Object.freeze({intake: {config: {active_features: ['advanced_search']}}})
-      expect(isAdvancedSearchOn({pathname: '/screenings/123/edit'})).toEqual(true)
+      expect(isAdvancedSearchOn({pathname: '/screenings/123/edit'})).toEqual(false)
     })
 
     it('display advanced search when path include snapshot', () => {
       window.org = Object.freeze({intake: {config: {active_features: ['advanced_search']}}})
       expect(isAdvancedSearchOn({pathname: '/snapshot'})).toEqual(true)
+    })
+  })
+
+  describe('.isSearchByAddressOn', () => {
+    it('returns based on hotline flag when path includes screenings', () => {
+      window.org = Object.freeze({intake: {config: {active_features: ['address_search_hotline']}}})
+      expect(isSearchByAddressOn({pathname: '/screenings/123/edit'})).toEqual(true)
+
+      window.org = Object.freeze({intake: {config: {active_features: ['address_search_snapshot']}}})
+      expect(isSearchByAddressOn({pathname: '/screenings/123/edit'})).toEqual(false)
+    })
+
+    it('returns based on snapshot flag when path does not include screenings', () => {
+      window.org = Object.freeze({intake: {config: {active_features: ['address_search_hotline']}}})
+      expect(isSearchByAddressOn({pathname: '/snapshot'})).toEqual(false)
+
+      window.org = Object.freeze({intake: {config: {active_features: ['address_search_snapshot']}}})
+      expect(isSearchByAddressOn({pathname: '/snapshot'})).toEqual(true)
     })
   })
 })
