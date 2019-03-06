492b5caa9fc1981e374912bb524a9c5acb3ef737
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-03-05 21:04:21 -0800

    Add fuzzy match for first name and multi match for name inverse;

--- spec/services/person_search_name_query_builder_spec.rb
+++ spec/services/person_search_name_query_builder_spec.rb
@@ -8,8 +8,10 @@ describe PersonSearchNameQueryBuilder do
     let(:first_name) { 'first name' }
     let(:middle_name) { 'middle name' }
     let(:suffix) { 'suffix' }
-    let(:name_query) { PersonSearchResultBuilder.new.name_query }
-    let(:params) do
+    let(:full_name_query) { PersonSearchResultBuilder.new.full_name_query }
+    let(:last_name_query) { PersonSearchResultBuilder.new.last_name_query }
+    let(:first_name_query) { PersonSearchResultBuilder.new.first_name_query }
+    let(:full_name_params) do
       {
         last_name: last_name,
         first_name: first_name,
@@ -17,12 +19,40 @@ describe PersonSearchNameQueryBuilder do
         suffix: suffix
       }
     end
+    let(:last_name_params) do
+      {
+        last_name: last_name,
+        suffix: suffix
+      }
+    end
+    let(:first_name_params) do
+      {
+        first_name: first_name,
+        suffix: suffix
+      }
+    end
 
     context 'returns hash' do
-      it 'with query' do
-        query_builder = QueryBuilder.new(person_search_fields: params)
+      it 'with full name query' do
+        query_builder = QueryBuilder.new(person_search_fields: full_name_params)
         query = query_builder.extend(described_class).query
-        expect(query.as_json).to eq name_query['query']
+        expect(query.as_json).to eq full_name_query['query']
+      end
+    end
+
+    context 'returns hash' do
+      it 'with last name query' do
+        query_builder = QueryBuilder.new(person_search_fields: last_name_params)
+        query = query_builder.extend(described_class).query
+        expect(query.as_json).to eq last_name_query['query']
+      end
+    end
+
+    context 'returns hash' do
+      it 'with first name query' do
+        query_builder = QueryBuilder.new(person_search_fields: first_name_params)
+        query = query_builder.extend(described_class).query
+        expect(query.as_json).to eq first_name_query['query']
       end
     end
   end
--- spec/support/helpers/person_search_name_query_builder_helper.rb
+++ spec/support/helpers/person_search_name_query_builder_helper.rb
@@ -2,7 +2,7 @@
 
 # rubocop:disable Metrics/ModuleLength
 module PersonSearchNameQueryBuilderHelper
-  def name_query
+  def full_name_query
     {
       "size": '10',
       "track_scores": 'true',
@@ -24,7 +24,7 @@ module PersonSearchNameQueryBuilderHelper
           "should": [
             {
               "query_string": {
-                "boost": '4',
+                "boost": '10',
                 "default_field": 'last_name',
                 "query": 'last name'
               }
@@ -38,7 +38,7 @@ module PersonSearchNameQueryBuilderHelper
             },
             {
               "query_string": {
-                "boost": '4',
+                "boost": '2',
                 "default_field": 'middle_name',
                 "query": 'middle name'
               }
@@ -83,6 +83,81 @@ module PersonSearchNameQueryBuilderHelper
                 "boost": '2',
                 "default_field": 'middle_name.diminutive',
                 "query": 'middle name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '4',
+                "default_field": 'name_suffix',
+                "query": 'suffix'
+              }
+            },
+            {
+              "multi_match": {
+                "query": 'last name first name',
+                "type": 'cross_fields',
+                "fields": %w[first_name last_name],
+                "operator": 'and',
+                "boost": '10'
+              }
+            },
+            {
+              "match": {
+                "first_name": {
+                  "query": 'first name',
+                  "operator": 'and',
+                  "boost": '1',
+                  "fuzziness": '3'
+                }
+              }
+            }
+          ]
+        }
+      },
+      "_source": source,
+      "highlight": highlight
+    }.as_json
+  end
+
+  def last_name_query
+    {
+      "size": '10',
+      "track_scores": 'true',
+      "sort": [
+        {
+          "_score": 'desc',
+          "_uid": 'desc'
+        }
+      ],
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "match": {
+                "legacy_descriptor.legacy_table_name": 'CLIENT_T'
+              }
+            }
+          ],
+          "should": [
+            {
+              "query_string": {
+                "boost": '10',
+                "default_field": 'last_name',
+                "query": 'last name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'last_name.phonetic',
+                "query": 'last name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'last_name.diminutive',
+                "query": 'last name'
               }
             },
             {
@@ -100,7 +175,7 @@ module PersonSearchNameQueryBuilderHelper
     }.as_json
   end
 
-  def name_ssn_query
+  def first_name_query
     {
       "size": '10',
       "track_scores": 'true',
@@ -123,6 +198,72 @@ module PersonSearchNameQueryBuilderHelper
             {
               "query_string": {
                 "boost": '4',
+                "default_field": 'first_name',
+                "query": 'first name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'first_name.phonetic',
+                "query": 'first name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'first_name.diminutive',
+                "query": 'first name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '4',
+                "default_field": 'name_suffix',
+                "query": 'suffix'
+              }
+            },
+            {
+              "match": {
+                "first_name": {
+                  "query": 'first name',
+                  "operator": 'and',
+                  "boost": '1',
+                  "fuzziness": '3'
+                }
+              }
+            }
+          ]
+        }
+      },
+      "_source": source,
+      "highlight": highlight
+    }.as_json
+  end
+
+  def full_name_ssn_query
+    {
+      "size": '10',
+      "track_scores": 'true',
+      "sort": [
+        {
+          "_score": 'desc',
+          "_uid": 'desc'
+        }
+      ],
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "match": {
+                "legacy_descriptor.legacy_table_name": 'CLIENT_T'
+              }
+            }
+          ],
+          "should": [
+            {
+              "query_string": {
+                "boost": '10',
                 "default_field": 'last_name',
                 "query": 'last name'
               }
@@ -136,7 +277,7 @@ module PersonSearchNameQueryBuilderHelper
             },
             {
               "query_string": {
-                "boost": '4',
+                "boost": '2',
                 "default_field": 'middle_name',
                 "query": 'middle name'
               }
@@ -188,6 +329,25 @@ module PersonSearchNameQueryBuilderHelper
                 "boost": '4',
                 "default_field": 'name_suffix',
                 "query": 'suffix'
+              }
+            },
+            {
+              "multi_match": {
+                "query": 'last name first name',
+                "type": 'cross_fields',
+                "fields": %w[first_name last_name],
+                "operator": 'and',
+                "boost": '10'
+              }
+            },
+            {
+              "match": {
+                "first_name": {
+                  "query": 'first name',
+                  "operator": 'and',
+                  "boost": '1',
+                  "fuzziness": '3'
+                }
               }
             },
             {
@@ -205,7 +365,7 @@ module PersonSearchNameQueryBuilderHelper
     }.as_json
   end
 
-  def name_date_of_birth_query
+  def full_name_date_of_birth_query
     {
       "size": '10',
       "track_scores": 'true',
@@ -234,7 +394,7 @@ module PersonSearchNameQueryBuilderHelper
           "should": [
             {
               "query_string": {
-                "boost": '4',
+                "boost": '10',
                 "default_field": 'last_name',
                 "query": 'last name'
               }
@@ -248,7 +408,7 @@ module PersonSearchNameQueryBuilderHelper
             },
             {
               "query_string": {
-                "boost": '4',
+                "boost": '2',
                 "default_field": 'middle_name',
                 "query": 'middle name'
               }
@@ -300,6 +460,25 @@ module PersonSearchNameQueryBuilderHelper
                 "boost": '4',
                 "default_field": 'name_suffix',
                 "query": 'suffix'
+              }
+            },
+            {
+              "multi_match": {
+                "query": 'last name first name',
+                "type": 'cross_fields',
+                "fields": %w[first_name last_name],
+                "operator": 'and',
+                "boost": '10'
+              }
+            },
+            {
+              "match": {
+                "first_name": {
+                  "query": 'first name',
+                  "operator": 'and',
+                  "boost": '1',
+                  "fuzziness": '3'
+                }
               }
             }
           ]
@@ -310,7 +489,7 @@ module PersonSearchNameQueryBuilderHelper
     }.as_json
   end
 
-  def name_ssn_date_of_birth_query
+  def full_name_ssn_date_of_birth_query
     {
       "size": '10',
       "track_scores": 'true',
@@ -339,7 +518,7 @@ module PersonSearchNameQueryBuilderHelper
           "should": [
             {
               "query_string": {
-                "boost": '4',
+                "boost": '10',
                 "default_field": 'last_name',
                 "query": 'last name'
               }
@@ -353,7 +532,7 @@ module PersonSearchNameQueryBuilderHelper
             },
             {
               "query_string": {
-                "boost": '4',
+                "boost": '2',
                 "default_field": 'middle_name',
                 "query": 'middle name'
               }
@@ -405,6 +584,25 @@ module PersonSearchNameQueryBuilderHelper
                 "boost": '4',
                 "default_field": 'name_suffix',
                 "query": 'suffix'
+              }
+            },
+            {
+              "multi_match": {
+                "query": 'last name first name',
+                "type": 'cross_fields',
+                "fields": %w[first_name last_name],
+                "operator": 'and',
+                "boost": '10'
+              }
+            },
+            {
+              "match": {
+                "first_name": {
+                  "query": 'first name',
+                  "operator": 'and',
+                  "boost": '1',
+                  "fuzziness": '3'
+                }
               }
             },
             {
@@ -422,7 +620,7 @@ module PersonSearchNameQueryBuilderHelper
     }.as_json
   end
 
-  def name_ssn_date_of_birth_query_address
+  def full_name_ssn_date_of_birth_address_query
     {
       "size": '10',
       "track_scores": 'true',
@@ -451,7 +649,7 @@ module PersonSearchNameQueryBuilderHelper
           "should": [
             {
               "query_string": {
-                "boost": '4',
+                "boost": '10',
                 "default_field": 'last_name',
                 "query": 'last name'
               }
@@ -465,7 +663,7 @@ module PersonSearchNameQueryBuilderHelper
             },
             {
               "query_string": {
-                "boost": '4',
+                "boost": '2',
                 "default_field": 'middle_name',
                 "query": 'middle name'
               }
@@ -517,6 +715,295 @@ module PersonSearchNameQueryBuilderHelper
                 "boost": '4',
                 "default_field": 'name_suffix',
                 "query": 'suffix'
+              }
+            },
+            {
+              "multi_match": {
+                "query": 'last name first name',
+                "type": 'cross_fields',
+                "fields": %w[first_name last_name],
+                "operator": 'and',
+                "boost": '10'
+              }
+            },
+            {
+              "match": {
+                "first_name": {
+                  "query": 'first name',
+                  "operator": 'and',
+                  "boost": '1',
+                  "fuzziness": '3'
+                }
+              }
+            },
+            {
+              "query_string": {
+                "default_field": 'ssn',
+                "query": '123456789',
+                "boost": '10'
+              }
+            },
+            {
+              "nested": {
+                "path": 'addresses',
+                "query": {
+                  "bool": {
+                    "should": [
+                      {
+                        "match": {
+                          "addresses.autocomplete_searchable_address": {
+                            "boost": '10',
+                            "operator": 'and',
+                            "query": 'street_number_and_name_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.last_known": {
+                            "boost": '10',
+                            "query": 'true'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.autocomplete_city": {
+                            "boost": '10',
+                            "query": 'city_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.county.description": {
+                            "boost": '10',
+                            "query": 'county_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.searchable_address": {
+                            "boost": '10',
+                            "query": 'street_number_and_name_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.city": {
+                            "boost": '10',
+                            "query": 'city_search_term'
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+          ]
+        }
+      },
+      "_source": source,
+      "highlight": highlight
+    }.as_json
+  end
+
+  def last_name_ssn_date_of_birth_address_query
+    {
+      "size": '10',
+      "track_scores": 'true',
+      "sort": [
+        {
+          "_score": 'desc',
+          "_uid": 'desc'
+        }
+      ],
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "match": {
+                "legacy_descriptor.legacy_table_name": 'CLIENT_T'
+              }
+            },
+            {
+              "query_string": {
+                "default_field": 'date_of_birth_as_text',
+                "query": '05051989',
+                "boost": '10'
+              }
+            }
+          ],
+          "should": [
+            {
+              "query_string": {
+                "boost": '10',
+                "default_field": 'last_name',
+                "query": 'last name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'last_name.phonetic',
+                "query": 'last name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'last_name.diminutive',
+                "query": 'last name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '4',
+                "default_field": 'name_suffix',
+                "query": 'suffix'
+              }
+            },
+            {
+              "query_string": {
+                "default_field": 'ssn',
+                "query": '123456789',
+                "boost": '10'
+              }
+            },
+            {
+              "nested": {
+                "path": 'addresses',
+                "query": {
+                  "bool": {
+                    "should": [
+                      {
+                        "match": {
+                          "addresses.autocomplete_searchable_address": {
+                            "boost": '10',
+                            "operator": 'and',
+                            "query": 'street_number_and_name_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.last_known": {
+                            "boost": '10',
+                            "query": 'true'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.autocomplete_city": {
+                            "boost": '10',
+                            "query": 'city_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.county.description": {
+                            "boost": '10',
+                            "query": 'county_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.searchable_address": {
+                            "boost": '10',
+                            "query": 'street_number_and_name_search_term'
+                          }
+                        }
+                      },
+                      {
+                        "match": {
+                          "addresses.city": {
+                            "boost": '10',
+                            "query": 'city_search_term'
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+          ]
+        }
+      },
+      "_source": source,
+      "highlight": highlight
+    }.as_json
+  end
+
+  def first_name_ssn_date_of_birth_address_query
+    {
+      "size": '10',
+      "track_scores": 'true',
+      "sort": [
+        {
+          "_score": 'desc',
+          "_uid": 'desc'
+        }
+      ],
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "match": {
+                "legacy_descriptor.legacy_table_name": 'CLIENT_T'
+              }
+            },
+            {
+              "query_string": {
+                "default_field": 'date_of_birth_as_text',
+                "query": '05051989',
+                "boost": '10'
+              }
+            }
+          ],
+          "should": [
+            {
+              "query_string": {
+                "boost": '4',
+                "default_field": 'first_name',
+                "query": 'first name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'first_name.phonetic',
+                "query": 'first name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '2',
+                "default_field": 'first_name.diminutive',
+                "query": 'first name'
+              }
+            },
+            {
+              "query_string": {
+                "boost": '4',
+                "default_field": 'name_suffix',
+                "query": 'suffix'
+              }
+            },
+            {
+              "match": {
+                "first_name": {
+                  "query": 'first name',
+                  "operator": 'and',
+                  "boost": '1',
+                  "fuzziness": '3'
+                }
               }
             },
             {
