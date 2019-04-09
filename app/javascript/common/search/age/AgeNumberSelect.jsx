34914f3628af715171c7a6cb906f30b698d76c5d
mzemlyanoi <Mykhailo.Zemlianyi@osi.ca.gov>
2019-04-09 10:18:07 -0700

    SNAP-1032 If DOB is selected, then Approximate age field should be disabled and previous contents erased. If Approximate age is selected, then DOB field should be disabled and previous contents erased.

--- app/javascript/common/search/age/AgeNumberSelect.jsx
+++ app/javascript/common/search/age/AgeNumberSelect.jsx
@@ -21,7 +21,6 @@ class AgeNumberSelect extends React.Component {
     const {id, gridClassName, value, range, disabled} = this.props
     const options = []
     if (disabled) this.props.onChange('searchApproximateAge', '')
-
     for (let x = range.min; x <= range.max; x++) {
       const option = (<option key={x} value={x}>{x}</option>)
       options.push(option)
