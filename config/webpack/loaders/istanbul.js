cf7ad85e19dfaaca11f8f51f04465e42d86a17ef
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2019-04-26 15:56:01 -0700

    FIT-571 remove istanbul cverage config to resolve conflicting webpack

--- config/webpack/loaders/istanbul.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  test: /\.js$|\.jsx$/,
-  enforce: 'post',
-  loader: 'istanbul-instrumenter-loader',
-  exclude: /node_modules|spec|public/,
-  query: {
-      esModules: true
-  }
-}
