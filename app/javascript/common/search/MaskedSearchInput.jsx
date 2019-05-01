2acca77a8a85d199f7ad5b083f279217c43322d7
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-05-01 10:59:17 -0700

    Move text formatters from MaskedsearchInput component to utils; Update tests;

--- app/javascript/common/search/MaskedSearchInput.jsx
+++ app/javascript/common/search/MaskedSearchInput.jsx
@@ -1,26 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import HyphenatedMaskedInput from 'common/HyphenatedMaskedInput'
-
-const capitalizedStr = str =>
-  `${str.charAt(0).toUpperCase()}${str.slice(1)}`
-
-const toCamelCase = name => {
-  const subStrArray = name.split(/-/g)
-  const firstSubStr = subStrArray[0].toLowerCase()
-  const subStringsAfterFirst = subStrArray.slice(1)
-  const capitalizedSubStrings = subStringsAfterFirst.map(substr => capitalizedStr(substr))
-  return `${firstSubStr}${capitalizedSubStrings.join('')}`
-}
-
-const toCapitalCase = name => name
-  .split(/-/g)
-  .map(substr => capitalizedStr(substr))
-  .join('')
+import {toCamelCase, toCapitalCase} from 'utils/textFormatter'
 
 const MaskedSearchInput = ({name, onBlur, onChange, onFocus, ...props}) => {
-  const camelName = toCamelCase(name)
-  const capitalName = toCapitalCase(name)
+  const camelName = toCamelCase(name, /-/g)
+  const capitalName = toCapitalCase(name, /-/g)
   return (
     <HyphenatedMaskedInput
       id={`search-${name}`}
--- spec/javascripts/utils/textFormatterSpec.js
+++ spec/javascripts/utils/textFormatterSpec.js
@@ -1,4 +1,4 @@
-import {nWords} from 'utils/textFormatter'
+import {nWords, capitalizedStr, toCamelCase, toCapitalCase} from 'utils/textFormatter'
 
 describe('nWords', () => {
   const threeWords = 'word word word'
@@ -31,5 +31,43 @@ describe('nWords', () => {
 
   it('returns undefined if the text is undefined', () => {
     expect(nWords(undefined, 1)).toEqual(undefined)
+  })
+})
+
+describe('capitalizedStr', () => {
+  // the method assumes the string is already lower-cased
+  it('capitalizes the first character of a string', () => {
+    const str = 'text'
+    expect(capitalizedStr(str)).toEqual('Text')
+  })
+})
+
+describe('toCamelCase', () => {
+  // this method assumes the string is already lower-cased
+  it('camel-cases a string with dashes', () => {
+    const str = 'camel-case-text'
+    const seperator = /-/g
+    expect(toCamelCase(str, seperator)).toEqual('camelCaseText')
+  })
+
+  it('camel-cases a string with periods', () => {
+    const str = 'camel.case.text'
+    const seperator = /\./g
+    expect(toCamelCase(str, seperator)).toEqual('camelCaseText')
+  })
+})
+
+describe('toCapitalCase', () => {
+  // this method assumes the string is already lower-cased
+  it('capital-cases a string with dashes', () => {
+    const str = 'capital-case-text'
+    const seperator = /-/g
+    expect(toCapitalCase(str, seperator)).toEqual('CapitalCaseText')
+  })
+
+  it('capital-cases a string with equal signs', () => {
+    const str = 'capital=case=text'
+    const seperator = /=/g
+    expect(toCapitalCase(str, seperator)).toEqual('CapitalCaseText')
   })
 })
