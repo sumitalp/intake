be02cbc85760a0481fe025a0f206c4a595ffde86
mzemlyanoi <Mykhailo.Zemlianyi@osi.ca.gov>
2019-05-09 13:19:27 -0700

    REGRESSION STAGE

--- spec/regression/index_page_spec.rb
+++ spec/regression/index_page_spec.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
-require 'acceptance_helpers'
 require 'feature'
 
 feature 'Index Page' do
   scenario 'CANS-Worker lands on client list and can logout' do
-    login
-    expect(page).to have_content('CANS')
-    expect(page).to have_content('Client List')
-    logout
+    visit '/'
+    expect(page).to have_content('INTAKE')
   end
 end
