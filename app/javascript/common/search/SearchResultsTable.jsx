217ce2eb18ff336d59e948d28112609b6c683b01
manish pokharel <manish.pokharel@osi.ca.gov>
2019-05-09 11:37:34 -0700

    SNAP-984 refactored search results table

--- app/javascript/common/search/SearchResultsTable.jsx
+++ app/javascript/common/search/SearchResultsTable.jsx
@@ -4,47 +4,47 @@ import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 
 class SearchResultsTable extends Component {
+  columns = [{
+    id: 'name',
+    Header: 'Name',
+    Cell: (row) => {
+      const personName = row.original.fullName
+      return personName ?
+        `${row.index + 1}. ${personName}` : ''
+    },
+  }, {
+    Header: 'Date of Birth',
+    accessor: 'dateOfBirth',
+  }, {
+    Header: 'Sex at Birth',
+    accessor: 'gender',
+  }, {
+    Header: 'Service Provider County',
+    accessor: null,
+  }, {
+    Header: 'Service Provider Phone',
+    accessor: null,
+  }, {
+    id: 'address',
+    Header: 'Address',
+    accessor: results => {
+      const personAddress = results.address
+      return personAddress ?
+        `${personAddress.streetAddress},
+         ${personAddress.city},
+         ${personAddress.state},
+         ${personAddress.zip}` : ''
+    },
+  }, {
+    Header: 'Case Status',
+    accessor: null,
+  }]
   render() {
     const {results} = this.props
-    const columns = [{
-      id: 'name',
-      Header: 'Name',
-      Cell: (row) => {
-        const personName = row.original.fullName
-        return personName ?
-          `${row.index + 1}. ${personName}` : ''
-      },
-    }, {
-      Header: 'Date of Birth',
-      accessor: 'dateOfBirth',
-    }, {
-      Header: 'Sex at Birth',
-      accessor: 'gender',
-    }, {
-      Header: 'Service Provider County',
-      accessor: null,
-    }, {
-      Header: 'Service Provider Phone',
-      accessor: null,
-    }, {
-      id: 'address',
-      Header: 'Address',
-      accessor: results => {
-        const personAddress = results.address
-        return personAddress ?
-          `${personAddress.streetAddress},
-           ${personAddress.city},
-           ${personAddress.state},
-           ${personAddress.zip}` : ''
-      },
-    }, {
-      Header: 'Case Status',
-      accessor: null,
-    }]
     return (
       <ReactTable
         data={results}
-        columns={columns}
+        columns={this.columns}
         defaultPageSize={25}
         minRows={0}
       />
