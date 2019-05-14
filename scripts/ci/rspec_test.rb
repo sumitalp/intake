6506cb267e1af73b882fa9219278be26533dc455
mzemlyanoi <Mykhailo.Zemlianyi@osi.ca.gov>
2019-05-13 20:13:03 -0700

    REGRESSION STAGE

--- scripts/ci/rspec_test.rb
+++ scripts/ci/rspec_test.rb
@@ -5,9 +5,8 @@ test_args = "-p intake_accelerator#{ENV['BUILD_NAME']}_test -f docker/test/docke
 webpack = 'bin/webpack'
 folder = ' spec'
 command = '--exclude-pattern '
-regr = ',./regression'
 
 rspc_cmd = "#{webpack} && bundle exec parallel_rspec --runtime-log parallel_runtime_rspec.log "
-rspc_cmd += (ENV['EXCLUDE_PATTERN'] ? command + ENV['EXCLUDE_PATTERN'] + regr : '') + folder
+rspc_cmd += (ENV['EXCLUDE_PATTERN'] ? command + ENV['EXCLUDE_PATTERN'] : '') + folder
 
 exec("docker-compose #{test_args} run run_tests bash -c \"#{rspc_cmd}\"")
