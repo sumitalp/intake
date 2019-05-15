095f9fefc50e62f87989b129b1253d69b65dcff4
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2019-05-15 16:52:10 -0700

    FIT-598 cleanup dead code due to react-wood-duck

--- app/javascript/common/GrouperHeading.jsx
+++ app/javascript/common/GrouperHeading.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-// This tiny component should eventually move to react-wood-duck
 const GrouperHeading = ({text}) => (
   <h4 className='grouper'>{text}</h4>
 )
