0c06e974841c038b92d6b82c32d54b2f6c15644f
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-05-20 10:49:41 -0700

    push to snapshot page in createSnapshotSaga on createSnapshot();

--- app/javascript/sagas/createSnapshotSaga.js
+++ app/javascript/sagas/createSnapshotSaga.js
@@ -1,9 +1,11 @@
 import {takeEvery, put} from 'redux-saga/effects'
 import {clearSnapshot} from 'actions/snapshotActions'
 import {CREATE_SNAPSHOT} from 'actions/actionTypes'
+import {push} from 'react-router-redux'
 
 export function* createSnapshot() {
   yield put(clearSnapshot())
+  yield put(push('/snapshot'))
 }
 export function* createSnapshotSaga() {
   yield takeEvery(CREATE_SNAPSHOT, createSnapshot)
--- spec/javascripts/sagas/createSnapshotSagaSpec.js
+++ spec/javascripts/sagas/createSnapshotSagaSpec.js
@@ -8,6 +8,7 @@ import {CREATE_SNAPSHOT} from 'actions/actionTypes'
 import {
   clearSnapshot,
 } from 'actions/snapshotActions'
+import {push} from 'react-router-redux'
 
 describe('createSnapshotSaga', () => {
   it('creates snapshot on CREATE_SNAPSHOT', () => {
@@ -21,6 +22,9 @@ describe('createSnapshot', () => {
     const gen = createSnapshot()
     expect(gen.next().value).toEqual(
       put(clearSnapshot())
+    )
+    expect(gen.next().value).toEqual(
+      put(push('/snapshot'))
     )
   })
 })
